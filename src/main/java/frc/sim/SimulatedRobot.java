22eb175f9e1ee1e42894f86fdfd941643880d371
KianChilds <94756266+KianChilds@users.noreply.github.com>
2026-08-21 10:16:19 -0500

    Added Simulation Check

--- src/main/java/frc/sim/SimulatedRobot.java
+++ src/main/java/frc/sim/SimulatedRobot.java
@@ -5,17 +5,24 @@ import static edu.wpi.first.units.Units.Seconds;
 import java.util.function.Supplier;
 
 import edu.wpi.first.wpilibj.TimedRobot;
+import frc.robot.Robot;
 
 public class SimulatedRobot {
     public static Supplier<TimedRobot> createSimRobot(Supplier<TimedRobot> robotSupplier, Supplier<SimMain> simSupplier){
         return () -> {
             TimedRobot newRobot = robotSupplier.get();
+            
+            if(Robot.isReal()){
+                return newRobot;
+            }
+
             SimMain sim = simSupplier.get();
+
 
             // Simulation should be ran at a frequency of 100hz
             newRobot.addPeriodic(sim::physicsPeriodic, sim.physics_dt.in(Seconds));
             newRobot.addPeriodic(sim::NTPeriodic, sim.nt_dt.in(Seconds));
-            
+
             return newRobot;
         };
     }
