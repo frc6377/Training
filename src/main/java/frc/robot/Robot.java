f96807ebea483e04e8d9d1e067536f357228ba1b
aterrel92 <aterrel92@gmail.com>
2026-08-29 11:15:43 -0500

    added tank drive

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,32 +4,63 @@
 
 package frc.robot;
 
+import java.lang.ModuleLayer.Controller;
+
+import com.ctre.phoenix6.hardware.TalonFX;
+
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.motorcontrol.Talon;
 
 /**
  * The methods in this class are called automatically corresponding to each mode, as described in
  * the TimedRobot documentation. If you change the name of this class or the package after creating
  * this project, you must also update the Main.java file in the project.
  */
-public class Robot extends TimedRobot {
-
+public class Robot extends TimedRobot{
+  private final XboxController driver;
+  private final TalonFX rightMotorSystem;
+  private final TalonFX leftMotorSystem;
   /**
    * This function is run when the robot is first started up and should be used for any
    * initialization code.
-   */
-  public Robot() {}
+*/
+  public Robot() {
+  driver = new XboxController(0);
+  rightMotorSystem = new TalonFX(1);
+  leftMotorSystem = new TalonFX(3);
+  }
 
   @Override
-  public void robotPeriodic() {}
+  public void robotPeriodic() {
+    double forward = -driver.getLeftY();
+    double right = .775*driver.getRightX();
+double rightSpeed = forward + right;
+double leftSpeed = forward - right;
+rightMotorSystem.set(rightSpeed);
+leftMotorSystem.set(leftSpeed);
+if (forward == 0) {
+  right = right / 4;
+}
+
+
+
+
+    //Controller.
+  }
 
   @Override
   public void autonomousInit() {}
 
   @Override
-  public void autonomousPeriodic() {}
+  public void autonomousPeriodic() {
+
+
+  }
 
   @Override
-  public void teleopInit() {}
+  public void teleopInit() {
+  }
 
   @Override
   public void teleopPeriodic() {}
@@ -51,4 +82,4 @@ public class Robot extends TimedRobot {
 
   @Override
   public void simulationPeriodic() {}
-}
+
